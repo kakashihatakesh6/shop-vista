327c73f3b9eb79e6c048e0cfc1906eac539bcb0b
Nikhil Dasar <kakashihatakesh6@gmail.com>
2024-01-20 19:35:29 +0530

    third deploy

--- pages/admin/compo/layoutt/header/data.js
+++ pages/admin/compo/layoutt/header/data.js
@@ -1,21 +1,3 @@
-import img1 from "src/assets/images/profile/user-1.jpg"
-import img2 from "src/assets/images/profile/user-2.jpg"
-import img3 from "src/assets/images/profile/user-3.jpg"
-import img4 from "src/assets/images/profile/user-4.jpg"
-
-import icon1 from "src/assets/images/svgs/icon-account.svg"
-import icon2 from "src/assets/images/svgs/icon-inbox.svg"
-import icon3 from "src/assets/images/svgs/icon-tasks.svg"
-
-import ddIcon1 from "src/assets/images/svgs/icon-dd-chat.svg"
-import ddIcon2 from "src/assets/images/svgs/icon-dd-cart.svg"
-import ddIcon3 from "src/assets/images/svgs/icon-dd-invoice.svg"
-import ddIcon4 from "src/assets/images/svgs/icon-dd-date.svg"
-import ddIcon5 from "src/assets/images/svgs/icon-dd-mobile.svg"
-import ddIcon6 from "src/assets/images/svgs/icon-dd-lifebuoy.svg"
-import ddIcon7 from "src/assets/images/svgs/icon-dd-message-box.svg"
-import ddIcon8 from "src/assets/images/svgs/icon-dd-application.svg"
-
 //
 // Notifications dropdown
 //
