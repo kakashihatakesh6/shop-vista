a169060a3f415da4e943c16e10440ee483723d7c
Nikhil Dasar <kakashihatakesh6@gmail.com>
2024-01-20 19:31:25 +0530

    second deploy

---
+++ pages/admin/compo/dashboard/DailyActivity.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DailyActivity = () => {
+  return (
+    <div>DailyActivity</div>
+  )
+}
+
+export default DailyActivity
---
+++ pages/admin/compo/dashboard/ProductPerformance.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductPerformance = () => {
+  return (
+    <div>ProductPerformance</div>
+  )
+}
+
+export default ProductPerformance
